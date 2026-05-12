2f57581a9c50fac179e1f04c077cbf6d77652a0b
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-12 12:09:16 -0500

    commit - ProductoDetalle .

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,17 +1,23 @@
-﻿namespace SOAP_PRODUCTOS.Models
+﻿using System.Runtime.Serialization;
+
+namespace SOAP_PRODUCTOS.Models
 {
+    [DataContract]
     public class Producto
     {
+        [DataMember]
         public int IdProducto { get; set; }
 
+        [DataMember]
         public string Nombre { get; set; }
 
+        [DataMember]
         public string Categoria { get; set; }
 
+        [DataMember]
         public DateTime FechaRegistro { get; set; }
 
-        // INICIALIZAR LISTA
-
+        [DataMember]
         public List<ProductoDetalle> Detalles
         {
             get;
--- Models/ProductoDetalle.cs
+++ Models/ProductoDetalle.cs
@@ -1,13 +1,29 @@
-﻿namespace SOAP_PRODUCTOS.Models
+﻿using System.Runtime.Serialization;
+
+namespace SOAP_PRODUCTOS.Models
 {
+    [DataContract]
     public class ProductoDetalle
     {
+        [DataMember]
         public string Codigo { get; set; }
+
+        [DataMember]
         public string Nombre { get; set; }
+
+        [DataMember]
         public string Descripcion { get; set; }
+
+        [DataMember]
         public decimal Precio { get; set; }
+
+        [DataMember]
         public int Stock { get; set; }
+
+        [DataMember]
         public string Marca { get; set; }
+
+        [DataMember]
         public string Estado { get; set; }
     }
 }   
