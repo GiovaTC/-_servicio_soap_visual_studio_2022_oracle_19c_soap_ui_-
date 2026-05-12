af722a7176c3be0b63f5df460812404e7a9f06d3
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-12 11:55:45 -0500

    commit - Program .

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 using CoreWCF;
 using CoreWCF.Configuration;
+using CoreWCF.Description;
+
 using SOAP_PRODUCTOS.Contracts;
 using SOAP_PRODUCTOS.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddServiceModelServices();
 
-builder.Services.AddSingleton<IProductoService, ProductoService>();
+builder.Services.AddServiceModelMetadata();
+
+builder.Services.AddSingleton<IProductoService,
+    ProductoService>();
 
 var app = builder.Build();
 
@@ -23,6 +29,11 @@ app.UseServiceModel(serviceBuilder =>
         new BasicHttpBinding(),
         "/ProductoService.svc"
     );
+
+    var smb =
+        app.Services.GetRequiredService<ServiceMetadataBehavior>();
+
+    smb.HttpGetEnabled = true;
 });
 
 app.Run();  
