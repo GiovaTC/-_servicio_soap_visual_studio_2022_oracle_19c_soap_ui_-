97714a6a0ff0c53594b5a0be3534d6b4199a02f3
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-12 11:28:19 -0500

    commit - Producto .

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -2,5 +2,10 @@
 {
     public class Producto
     {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public decimal Categoria { get; set; }
+        public String FechaRegistro { get; set; }
+        public List<ProductoDetalle> Detalles { get; set; }
     }
-}
+}   
