f44c51b977ab74654dabd71aff86537f23382208
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-12 11:30:26 -0500

    commit - ProductoDetalle .

--- Models/ProductoDetalle.cs
+++ Models/ProductoDetalle.cs
@@ -2,5 +2,12 @@
 {
     public class ProductoDetalle
     {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public string Marca { get; set; }
+        public string Estado { get; set; }
     }
-}
+}   
